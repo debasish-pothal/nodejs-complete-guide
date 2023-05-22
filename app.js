2ae8213fd785c1587e783f32cb1e3f1401736eb9
Debasish Pothal <debasish_pothal@hotmail.com>
2023-05-22 19:19:46 +0530

    understanding the basics
    sending get and post request

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const http = require("http");
+const fs = require("fs");
 
 function reqListener(req, res) {
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<p>hi there!</p>");
-  res.write("</html>");
-  res.end();
+  const url = req.url;
+  const method = req.method;
+
+  if (url === "/" && method === "GET") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<p>hi there!</p>");
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    console.log(req);
+    fs.writeFileSync("message.txt", "Test success");
+    res.statusCode = 302;
+    return res.end();
+  }
 }
 
 const server = http.createServer(reqListener);
