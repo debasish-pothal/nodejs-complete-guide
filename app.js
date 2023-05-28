67de6d4d59fc015d2102e58c585c8e0dd14862fd
Debasish Pothal <debasish_pothal@hotmail.com>
2023-05-28 17:24:47 +0530

    added views

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const path = require("path");
 const bodyParser = require("body-parser");
+
+const rootDir = require("./utils/path");
 
 const adminRoute = require("./routes/admin");
 const shopRoute = require("./routes/shop");
@@ -7,9 +10,14 @@ const shopRoute = require("./routes/shop");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(rootDir, "public")));
 
-app.use(adminRoute);
+app.use("/admin", adminRoute);
 
 app.use(shopRoute);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
+});
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,15 @@
 const express = require("express");
+const path = require("path");
+
+const rootDir = require("../utils/path");
 
 const router = express.Router();
 
 router.get("/add-product", (req, res, next) => {
-  res.send(
-    '<form method="POST" action="/user"><input type="text" name="title" /><button type="submit">Save</button></form>'
-  );
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
-router.post("/product", (req, res, next) => {
+router.post("/add-product", (req, res, next) => {
   console.log(req.body);
   res.redirect("/");
 });
